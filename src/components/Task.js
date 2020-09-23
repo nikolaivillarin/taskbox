bbe867da6a5c7b589d86e402abe6da4fb949a8c1
Nikolai Villarin <3136560+nikolaivillarin@users.noreply.github.com>
2020-09-23 16:42:47 -0400

    change task background to red

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { reducer } from '../lib/redux';
 
 export default function Task({ task: {id, title, state}, onArchiveTask, onPinTask }) {
   return (
@@ -22,6 +23,7 @@ export default function Task({ task: {id, title, state}, onArchiveTask, onPinTas
           value={title}
           readOnly={true}
           placeholder="Input title"
+          style={{ background: 'red' }}
         />
       </div>
       <div 
